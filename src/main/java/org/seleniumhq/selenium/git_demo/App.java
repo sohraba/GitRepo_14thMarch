414cd05aa272e4b3ff90d73b5248d22cabc93f14
sohdan090 <tech.desk090@gmail.com>
2023-03-14 22:34:49 +0530

    First Commit

---
+++ src/main/java/org/seleniumhq/selenium/git_demo/App.java
@@ -0,0 +1,22 @@
+package org.seleniumhq.selenium.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	int x,int y,int sum;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		App app=new App();
+		app.sum();
+    }
+	public void sum(){
+		System.out.println("sdsdad");
+int x=10;
+int y=20;
+int sum=x+y;
+	}
+}
---
+++ src/test/java/org/seleniumhq/selenium/git_demo/AppTest.java
@@ -0,0 +1,18 @@
+package org.seleniumhq.selenium.git_demo;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+   
+{
+  protected WebDriver driver;
+  @Test
+  public void testGoogle() throws InterruptedException{
+	  System.setProperty("webdriver.chrome.driver", "F:\\Backup\\MyCode\\chromedriver\\chromedriver.exe");
+  }
+  
+}
